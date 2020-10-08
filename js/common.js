58bbe1e1440ecd53d29fbdc4555c7cdefe305097
WangLifu <wanglifu@mapiz.com>
2020-10-08 21:35:28 +0800

    update course nav-bar

--- js/common.js
+++ js/common.js
@@ -1,43 +1,50 @@
+import TMPS from "../data/template.js";
 
-import TMPS from '../data/template.js'
-
-export function setActive(id) {
-  const ele = `#${id} li`
-  const first = $(ele)[0]
-  $(first).addClass('active')
+export function setActive(id, tag) {
+  const ele = `#${id} ${tag}`;
+  const first = $(ele)[0];
+  $(first).addClass("active");
   $(ele).click(function () {
-    $(this).siblings('li').removeClass('active');
-    $(this).addClass('active');
+    $(this).siblings(`${tag}`).removeClass("active");
+    $(this).addClass("active");
+  });
+}
+
+export function triggleButton(buttonId, id) {
+  const ele = `#${id}`;
+  const button = $(`#${buttonId}`);
+  $(ele).click(function () {
+    button.html($(this).text());
   });
 }
 
 export function appendElement(id, cb) {
-  const { tmp, list } = TMPS[id]
-  let el = $(`#${id}`)
-  let result = []
-  list.forEach(item => {
-    const tmpStr = replaceHolder(tmp)
-    Object.keys(item).forEach(key => {
-      tmpStr.replace(`$${key}`, item[key])
-    })
-    result.push($(tmpStr.getStr()))
-  })
-  el.append(result)
+  const { tmp, list } = TMPS[id];
+  let el = $(`#${id}`);
+  let result = [];
+  list.forEach((item) => {
+    const tmpStr = replaceHolder(tmp);
+    Object.keys(item).forEach((key) => {
+      tmpStr.replace(`$${key}`, item[key]);
+    });
+    result.push($(tmpStr.getStr()));
+  });
+  el.append(result);
   if (cb instanceof Function) {
-    cb()
+    cb();
   }
 }
 
 const replaceHolder = function (strTmp) {
-  var str = strTmp
+  var str = strTmp;
   function replace(key, val) {
-    str = str.replace(key, val)
+    str = str.replace(key, val);
   }
   function getStr() {
-    return str
+    return str;
   }
   return {
     replace: replace,
-    getStr: getStr
-  }
-}
+    getStr: getStr,
+  };
+};
--- js/render.js
+++ js/render.js
@@ -1,64 +1,64 @@
-import { appendElement, setActive } from './common.js'
+import { appendElement, setActive, triggleButton } from "./common.js";
 
 const IDS = {
   NAVBAR: {
-    ID: 'navbar-nav'
+    ID: "navbar-nav",
   },
   COURSE: {
     MODULE: {
-      ID: 'course-module',
-      ID_PHONE: 'course-module-phone'
+      ID: "course-module",
+      ID_PHONE: "course-module-phone",
+      ID_BUTTON: "course-module-button",
     },
     TIME: {
-      ID: 'course-time',
-      ID_PHONE: 'course-time-phone'
+      ID: "course-time",
+      ID_PHONE: "course-time-phone",
+      ID_BUTTON: "course-time-button",
     },
     TYPE: {
-      ID: 'course-type',
-      ID_PHONE: 'course-type-phone'
-    }
-  }
-}
+      ID: "course-type",
+      ID_PHONE: "course-type-phone",
+    },
+  },
+};
 
 // 渲染导航栏
 appendElement(IDS.NAVBAR.ID, () => {
   const current = location.pathname;
-  const ele = `#${IDS.NAVBAR.ID} a`
+  const ele = `#${IDS.NAVBAR.ID} a`;
   $(ele).each(function () {
     var $this = $(this);
-    if ($this.attr('href').indexOf(current) !== -1) {
-      $this.addClass('active')
+    if ($this.attr("href").indexOf(current) !== -1) {
+      $this.addClass("active");
     }
-  })
-})
-
-// 设置 active nav-bar 元素
-
+  });
+});
 
 // 渲染课程模块
 appendElement(IDS.COURSE.MODULE.ID, () => {
-  setActive(IDS.COURSE.MODULE.ID)
-})
+  setActive(IDS.COURSE.MODULE.ID, "li");
+});
 
-appendElement(IDS.COURSE.MODULE.ID_PHONE, () => {}
-)
+// 渲染课程模块 移动端
+appendElement(IDS.COURSE.MODULE.ID_PHONE, () => {
+  const BUTTON_ID = `${IDS.COURSE.MODULE.ID_BUTTON}`;
+  const ID = `${IDS.COURSE.MODULE.ID_PHONE} a`;
+  triggleButton(BUTTON_ID, ID);
+});
 
 // 渲染课程时间
 appendElement(IDS.COURSE.TIME.ID, () => {
-  setActive(IDS.COURSE.TIME.ID)
-})
+  setActive(IDS.COURSE.TIME.ID, "li");
+});
 
-appendElement(IDS.COURSE.TIME.ID_PHONE, () => {}
-)
+// 渲染课程时间 移动端
+appendElement(IDS.COURSE.TIME.ID_PHONE, () => {
+  const BUTTON_ID = `${IDS.COURSE.TIME.ID_BUTTON}`;
+  const ID = `${IDS.COURSE.TIME.ID_PHONE} a`;
+  triggleButton(BUTTON_ID, ID);
+});
 
 // 渲染课程类型
 appendElement(IDS.COURSE.TYPE.ID, () => {
-  setActive(IDS.COURSE.TYPE.ID)
-})
-
-// $(document).ready(function () {
-//   $('#navbar-nav a').click(function () {
-//     $(this).siblings('li').removeClass('active');
-//     $(this).addClass('active');
-//   });
-// });
+  setActive(IDS.COURSE.TYPE.ID, "li");
+});
